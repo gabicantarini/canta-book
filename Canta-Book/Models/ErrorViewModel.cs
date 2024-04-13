637a2d4a5f7c641282f885b23b8c527d0c7b15b7
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-04-13 22:00:37 +0100

    first commit

---
+++ Canta-Book/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Canta_Book.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
